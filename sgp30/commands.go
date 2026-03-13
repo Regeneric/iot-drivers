74612874a4ce81eb0f5d76b5cf634eaa1374cb08
Regeneric <hubek916@gmail.com>
2026-03-13 17:53:14 +0100

    Wrong logger was passed to Device

--- sgp30/commands.go
+++ sgp30/commands.go
@@ -9,7 +9,7 @@ import (
 
 func (d *Device) IaqInit() error {
 	log := d.log.With("func", "Device.IaqInit()", "params", "(-)", "return", "(error)", "lib", "sgp30")
-	log.Debug("[ SGP30 ] Sensor IAQ init;", "sensor", d.Config.Name)
+	log.Debug("[ SGP30 ] Sensor IAQ init", "sensor", d.Config.Name)
 
 	command := []uint8{uint8(CmdIaqInitMSB), uint8(CmdIaqInitLSB)}
 	if err := d.I2C.Tx(uint16(d.Config.Address), command, nil); err != nil {
@@ -19,13 +19,13 @@ func (d *Device) IaqInit() error {
 	// Table 10 Measurement commands
 	time.Sleep(10 * time.Millisecond)
 
-	log.Info("[ SGP30 ] Init command send to sensor;", "bus [", d.I2C.String(), "] address [", Hex8(d.Config.Address), "] command [", CmdIaqInit, "]")
+	log.Info("[ SGP30 ] Init command send to sensor", "bus [", d.I2C.String(), "] address [", Hex8(d.Config.Address), "] command [", CmdIaqInit, "]")
 	return nil
 }
 
 func (d *Device) MeasureIaq(data []uint8) error {
 	log := d.log.With("func", "Device.MeasureIaq()", "params", "([]uint8)", "return", "(error)", "lib", "sgp30")
-	log.Debug("[ SGP30 ] Measure eCO2 and TVOC values;", "sensor", d.Config.Name)
+	log.Debug("[ SGP30 ] Measure eCO2 and TVOC values", "sensor", d.Config.Name)
 
 	dataFrameLength := 6
 	if len(data) != dataFrameLength {
@@ -53,14 +53,14 @@ func (d *Device) MeasureIaq(data []uint8) error {
 		return errors.New("[ SGP30 ] TVOC CRC validation on '" + d.Config.Name + "' error: " + err.Error())
 	}
 
-	log.Info("[ SGP30 ] Measure command send to sensor;", "bus [", d.I2C.String(), "] address [", Hex8(d.Config.Address), "] command [", CmdMeasureIaq, "]")
+	log.Info("[ SGP30 ] Measure command send to sensor", "bus [", d.I2C.String(), "] address [", Hex8(d.Config.Address), "] command [", CmdMeasureIaq, "]")
 	return nil
 }
 
 // 6 bytes: (eCO2_MSB, eCO2_LSB, eCO2_CRC, TVOC_MSB, TVOC_LSB, TVOC_CRC)
 func (d *Device) GetIaqBaseline(data []uint8) error {
 	log := d.log.With("func", "Device.GetIaqBaseline()", "params", "([]uint8)", "return", "(error)", "lib", "sgp30")
-	log.Debug("[ SGP30 ] Get IAQ baseline calibration value;", "sensor", d.Config.Name)
+	log.Debug("[ SGP30 ] Get IAQ baseline calibration value", "sensor", d.Config.Name)
 
 	dataFrameLength := 6
 	if len(data) != dataFrameLength {
@@ -88,7 +88,7 @@ func (d *Device) GetIaqBaseline(data []uint8) error {
 		return errors.New("[ SGP30 ] TVOC CRC validation on '" + d.Config.Name + "' error: " + err.Error())
 	}
 
-	log.Info("[ SGP30 ] Get IAQ baseline command send to sensor;", "bus [", d.I2C.String(), "] address [", Hex8(d.Config.Address), "] command [", CmdGetIaqBaseline, "]")
+	log.Info("[ SGP30 ] Get IAQ baseline command send to sensor", "bus [", d.I2C.String(), "] address [", Hex8(d.Config.Address), "] command [", CmdGetIaqBaseline, "]")
 	return nil
 }
 
@@ -118,13 +118,13 @@ func (d *Device) SetIaqBaseline(data []uint8) error {
 	// Table 10 Measurement commands
 	time.Sleep(10 * time.Millisecond)
 
-	log.Info("[ SGP30 ] IAQ set baseline command send to sensor;", "bus [", d.I2C.String(), "] address [", Hex8(d.Config.Address), "] command [", CmdSetIaqBaseline, "]")
+	log.Info("[ SGP30 ] IAQ set baseline command send to sensor", "bus [", d.I2C.String(), "] address [", Hex8(d.Config.Address), "] command [", CmdSetIaqBaseline, "]")
 	return nil
 }
 
 func (d *Device) SetAbsoluteHumidity(data []uint8) error {
 	log := d.log.With("func", "Device.SetAbsoluteHumidity()", "params", "([]uint8)", "return", "(error)", "lib", "sgp30")
-	log.Debug("[ SGP30 ] Set absolute humidity calibration value;", "sensor", d.Config.Name)
+	log.Debug("[ SGP30 ] Set absolute humidity calibration value", "sensor", d.Config.Name)
 
 	dataFrameLength := 3
 	if len(data) != dataFrameLength {
@@ -144,14 +144,14 @@ func (d *Device) SetAbsoluteHumidity(data []uint8) error {
 	// Table 10 Measurement commands
 	time.Sleep(10 * time.Millisecond)
 
-	log.Info("[ SGP30 ] Absolute humidity calibration command send to sensor;", "bus [", d.I2C.String(), "] address [", Hex8(d.Config.Address), "] command [", CmdSetAbsoluteHumidity, "]")
+	log.Info("[ SGP30 ] Absolute humidity calibration command send to sensor", "bus [", d.I2C.String(), "] address [", Hex8(d.Config.Address), "] command [", CmdSetAbsoluteHumidity, "]")
 	return nil
 }
 
 // Page 10 Measure Test
 func (d *Device) MeasureTest(data []uint8) error {
 	log := d.log.With("func", "Device.MeasureTest()", "params", "([]uint8)", "return", "(error)", "lib", "sgp30")
-	log.Debug("[ SGP30 ] Sensor self-test;", "sensor", d.Config.Name)
+	log.Debug("[ SGP30 ] Sensor self-test", "sensor", d.Config.Name)
 
 	dataFrameLength := 3
 	if len(data) != dataFrameLength {
@@ -179,13 +179,13 @@ func (d *Device) MeasureTest(data []uint8) error {
 		return errors.New("[ SGP30 ] CRC validation on '" + d.Config.Name + "' error: " + err.Error())
 	}
 
-	log.Info("[ SGP30 ] Measure test command send to sensor;", "bus [", d.I2C.String(), "] address [", Hex8(d.Config.Address), "] command [", CmdMeasureTest, "]")
+	log.Info("[ SGP30 ] Measure test command send to sensor", "bus [", d.I2C.String(), "] address [", Hex8(d.Config.Address), "] command [", CmdMeasureTest, "]")
 	return nil
 }
 
 func (d *Device) GetFeatureSet(data []uint8) error {
 	log := d.log.With("func", "Device.GetFeatureSet()", "params", "([]uint8)", "return", "(error)", "lib", "sgp30")
-	log.Debug("[ SGP30 ] Get feature set;", "sensor", d.Config.Name)
+	log.Debug("[ SGP30 ] Get feature set", "sensor", d.Config.Name)
 
 	dataFrameLength := 3
 	if len(data) != dataFrameLength {
@@ -213,13 +213,13 @@ func (d *Device) GetFeatureSet(data []uint8) error {
 		return errors.New("[ SGP30 ] CRC validation on '" + d.Config.Name + "' error: " + err.Error())
 	}
 
-	log.Info("[ SGP30 ] Feature set command send to sensor;", "bus [", d.I2C.String(), "] address [", Hex8(d.Config.Address), "] command [", CmdGetFeatureSet, "]")
+	log.Info("[ SGP30 ] Feature set command send to sensor", "bus [", d.I2C.String(), "] address [", Hex8(d.Config.Address), "] command [", CmdGetFeatureSet, "]")
 	return nil
 }
 
 func (d *Device) MeasureRaw(data []uint8) error {
 	log := d.log.With("func", "Device.MeasureRaw()", "params", "([]uint8)", "return", "(error)", "lib", "sgp30")
-	log.Debug("[ SGP30 ] Measure raw sensor values;", "sensor", d.Config.Name)
+	log.Debug("[ SGP30 ] Measure raw sensor values", "sensor", d.Config.Name)
 
 	dataFrameLength := 6
 	if len(data) != dataFrameLength {
@@ -245,13 +245,13 @@ func (d *Device) MeasureRaw(data []uint8) error {
 		return errors.New("[ SGP30 ] C2H6O CRC validation on '" + d.Config.Name + "' error: " + err.Error())
 	}
 
-	log.Info("[ SGP30 ] Measure raw values command send to sensor;", "bus [", d.I2C.String(), "] address [", Hex8(d.Config.Address), "] command [", CmdMeasureRaw, "]")
+	log.Info("[ SGP30 ] Measure raw values command send to sensor", "bus [", d.I2C.String(), "] address [", Hex8(d.Config.Address), "] command [", CmdMeasureRaw, "]")
 	return nil
 }
 
 func (d *Device) GetTvocInceptiveBaseline(data []uint8) error {
 	log := d.log.With("func", "Device.GetTvocInceptiveBaseline()", "params", "([]uint8)", "return", "(error)", "lib", "sgp30")
-	log.Debug("[ SGP30 ] Inceptive baseline sensor values;", "sensor", d.Config.Name)
+	log.Debug("[ SGP30 ] Inceptive baseline sensor values", "sensor", d.Config.Name)
 
 	dataFrameLength := 3
 	if len(data) != dataFrameLength {
@@ -274,13 +274,13 @@ func (d *Device) GetTvocInceptiveBaseline(data []uint8) error {
 		return errors.New("[ SGP30 ] CRC validation on '" + d.Config.Name + "' error: " + err.Error())
 	}
 
-	log.Info("[ SGP30 ] Inceptive baseline set command send to sensor;", "bus [", d.I2C.String(), "] address [", Hex8(d.Config.Address), "] command [", CmdGetTvocInceptiveBaseline, "]")
+	log.Info("[ SGP30 ] Inceptive baseline set command send to sensor", "bus [", d.I2C.String(), "] address [", Hex8(d.Config.Address), "] command [", CmdGetTvocInceptiveBaseline, "]")
 	return nil
 }
 
 func (d *Device) SetTvocBaseline(data []uint8) error {
 	log := d.log.With("func", "Device.SetTvocBaseline()", "params", "([]uint8)", "return", "(error)", "lib", "sgp30")
-	log.Debug("[ SGP30 ] Set TVOC baseline calibration value;", "sensor", d.Config.Name)
+	log.Debug("[ SGP30 ] Set TVOC baseline calibration value", "sensor", d.Config.Name)
 
 	dataFrameLength := 2
 	if len(data) != dataFrameLength {
@@ -303,25 +303,25 @@ func (d *Device) SetTvocBaseline(data []uint8) error {
 	// Table 10 Measurement commands
 	time.Sleep(10 * time.Millisecond)
 
-	log.Info("[ SGP30 ] TVOC baseline calibration command send to sensor;", "bus [", d.I2C.String(), "] address [", Hex8(d.Config.Address), "] command [", CmdSetTvocBaseline, "]")
+	log.Info("[ SGP30 ] TVOC baseline calibration command send to sensor", "bus [", d.I2C.String(), "] address [", Hex8(d.Config.Address), "] command [", CmdSetTvocBaseline, "]")
 	return nil
 }
 
 func (d *Device) SoftReset() error {
 	log := d.log.With("func", "Device.SoftReset()", "params", "(-)", "return", "(error)", "lib", "sgp30")
-	log.Debug("[ SGP30 ] Sensor soft reset;", "sensor", d.Config.Name)
+	log.Debug("[ SGP30 ] Sensor soft reset", "sensor", d.Config.Name)
 
 	if err := d.I2C.Tx(uint16(CmdSoftResetMSB), []uint8{uint8(CmdSoftResetLSB)}, nil); err != nil {
 		return errors.New("[ SGP30 ] Could not send soft reset command to '" + d.Config.Name + "' sensor: " + err.Error())
 	}
 
-	log.Info("[ SGP30 ] Soft reset command send to sensor;", "bus [", d.I2C.String(), "] address [", Hex8(d.Config.Address), "] command [", CmdSoftReset, "]")
+	log.Info("[ SGP30 ] Soft reset command send to sensor", "bus [", d.I2C.String(), "] address [", Hex8(d.Config.Address), "] command [", CmdSoftReset, "]")
 	return nil
 }
 
 func (d *Device) GetSerialId(data []uint8) error {
 	log := d.log.With("func", "Device.GetSerialId()", "params", "(-)", "return", "(error)", "lib", "sgp30")
-	log.Debug("[ SGP30 ] Get sensor serial ID;", "sensor", d.Config.Name)
+	log.Debug("[ SGP30 ] Get sensor serial ID", "sensor", d.Config.Name)
 
 	dataFrameLength := 9
 	if len(data) != dataFrameLength {
@@ -350,7 +350,7 @@ func (d *Device) GetSerialId(data []uint8) error {
 		return errors.New("[ SGP30 ] CRC validation on '" + d.Config.Name + "' error: " + err.Error())
 	}
 
-	log.Info("[ SGP30 ] Get serial ID command send to sensor;", "bus [", d.I2C.String(), "] address [", Hex8(d.Config.Address), "] command [", CmdGetSerialId, "]")
+	log.Info("[ SGP30 ] Get serial ID command send to sensor", "bus [", d.I2C.String(), "] address [", Hex8(d.Config.Address), "] command [", CmdGetSerialId, "]")
 	return nil
 }
 
